41e9c0f4b84be1bfb898660f0c3b3bf05e5bd86d
Rahul <rahul967@hotmail.com>
2021-03-06 15:55:26 +0530

    Corrected issue of showing no deals available message while the data was stil loading, added a Spinner component with normal css

--- src/Pages/Deals.js
+++ src/Pages/Deals.js
@@ -3,18 +3,22 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import ProductGrid from "../Components/ProductGrid/ProductGrid";
 import ErrorComponent from "../Components/ErrorComponent/ErrorComponent";
+import Spinner from "../Components/Spinner/Spinner";
 
 const Deals = () => {
   const [items, setItems] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
 
   useEffect(() => {
     fetch(`https://gp-super-store-api.herokuapp.com/item/list/?isOnSale=true`)
       .then((res) => res.json())
       .then((data) => {
         setItems(data.items);
+        setisLoading(false);
       })
       .catch((error) => console.log(error));
   }, []);
+
   return (
     <div>
       <Navbar
@@ -24,10 +28,14 @@ const Deals = () => {
           { Cart: "/cart", id: 3 },
         ]}
       />
-      {items.length > 0 ? (
-        <ProductGrid data={items} />
+      {!isLoading ? (
+        items.length <= 0 ? (
+          <ErrorComponent message="No items on sale right now!" />
+        ) : (
+          <ProductGrid data={items} />
+        )
       ) : (
-        <ErrorComponent message="No items on sale right now!" />
+        <Spinner />
       )}
     </div>
   );
