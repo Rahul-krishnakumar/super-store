c99f3d6bc079e7baedd7cf0cca6d93ddc8c24333
Rahul <rahul967@hotmail.com>
2021-03-07 20:08:22 +0530

    Added search functionality

--- src/Components/ProductGrid/ProductGrid.js
+++ src/Components/ProductGrid/ProductGrid.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ProductCard from "../ProductCard/ProductCard";
 import SearchBar from "../SearchBar/SearchBar";
+import ErrorMessage from "../ErrorComponent/ErrorComponent";
 
 const ProductGrid = ({ data }) => {
+  const [products, setProducts] = useState(data);
+  console.log(products);
+
   return (
     <div className="xl:max-w-5xl md:max-w-2xl max-w-xs mx-auto my-10">
-      <SearchBar />
-      <div className="grid xl:grid-cols-3 gap-14 justify-items-center md:grid-cols-2 grid-cols-1">
-        {data.map((item) => {
-          return <ProductCard key={item["_id"]} itemData={item} />;
-        })}
-      </div>
+      <SearchBar search={(text) => setProducts(text)} items={data} />
+      {products.length > 0 ? (
+        <div className="grid xl:grid-cols-3 gap-14 justify-items-center md:grid-cols-2 grid-cols-1">
+          {products.map((item) => {
+            console.log(item);
+            return <ProductCard key={item["_id"]} itemData={item} />;
+          })}
+        </div>
+      ) : (
+        <ErrorMessage message="No such item exists!" />
+      )}
     </div>
   );
 };
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,7 +2,14 @@ import React from "react";
 
 import { FaSearch } from "react-icons/fa";
 
-const SearchBar = () => {
+const SearchBar = ({ search, items }) => {
+  const searchTerm = (e) => {
+    const result = items.filter((item) => {
+      return item["name"].toString().toLowerCase().startsWith(e.target.value);
+    });
+    search(result);
+  };
+
   return (
     <div className="-mt-4 mb-10">
       <form className="mx-auto flex justify-center gap-3">
@@ -10,6 +17,7 @@ const SearchBar = () => {
           className="w-60 border-b-2 border-gray-200 py-2 px-1 text-gray-700 placeholder-gray-400  leading-tight outline-none focus:border-blue-800"
           type="text"
           placeholder="Search"
+          onChange={searchTerm}
         />
         <button className="flex-shrink-0 bg-blue-700 border-blue-700 text-sm font-semibold border-4 text-blue-200 py-1 px-2 rounded hover:bg-blue-800 hover:border-blue-800">
           <FaSearch />
